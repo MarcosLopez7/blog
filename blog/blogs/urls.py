5bbfc3e166e81bf89f2b323f1461d478fc797a0a
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2021-02-17 21:39:37 -0600

    agregando templates para el blog

--- blog/blogs/urls.py
+++ blog/blogs/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:blog_id>/', views.retrieve_blog, name='retrieve_blog'),
 ]
--- blog/blogs/views.py
+++ blog/blogs/views.py
@@ -1,5 +1,15 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Blog
 
 def index(request):
     return HttpResponse("Hola mundo! estàs entrando en los blogs")
+
+
+def retrieve_blog(request, blog_id):
+    blog = Blog.objects.filter(id=blog_id)
+    context = {
+        "blog": blog
+    }
+    return render(request, 'polls/index.html', context)
