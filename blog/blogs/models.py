a2afac9780612108c2703f63b819569a8ed93c89
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2021-02-17 21:21:56 -0600

    creando el modelo blog y agregando el modelo al sitio del admin

--- blog/blogs/models.py
+++ blog/blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    """
+    TODO usar el objeto de User predefinido por Django
+    """
+    user = models.CharField(max_length=50, default="Usuario@test.com")
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
