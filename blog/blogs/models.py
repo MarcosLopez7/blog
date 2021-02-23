7714665cb01b7141b85133de66633f5e9c907236
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2021-02-22 19:09:25 -0600

    añadido objeto User de django

--- blog/blogs/models.py
+++ blog/blogs/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Blog(models.Model):
     """
     TODO usar el objeto de User predefinido por Django
     """
-    user = models.CharField(max_length=50, default="Usuario@test.com")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user = models.CharField(max_length=50, default="Usuario@test.com")
     title = models.CharField(max_length=100)
     image = models.ImageField(upload_to='static/img/')
     content = models.TextField()
