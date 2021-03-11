eed79ec45203b4b340657828a94fffeeb7c86cda
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2021-03-10 21:15:30 -0600

    agregando estilos generales

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from posts.models import Post
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+
+    context = {
+        'posts': posts
+    }
+
+    return render(request, 'index.html', context)
 
 def unauthorized(request):
     return render(request, 'unauthorized.html')
